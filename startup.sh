d888c0cc96e993cc7ae702f81c0c5d140bece010
Johannes Lorentzen <pippidis@gmail.com>
2023-01-14 19:09:58 +0100

    Startup tinkering

--- startup.sh
+++ startup.sh
@@ -1,11 +1,24 @@
-# Get latest from github (Overwriting this file)
+# To make this work, do the following: 
+# 1) Download this file to jpl root
+#   --> curl -o startup.sh https://raw.githubusercontet.com/pippidis/livingroom_automation/main/startup.sh
+# 2) sudo crontab -e
+#   --> Select nano
+# 3) add " @reboot startup.sh " to the file
+# 4) Save and exit
 
-curl -o repo.zip https://github.com/pippidis/livingroom_automation/archive/refs/heads/main.zip # Downloads the repo
-unzip -o repo.zip -d home/jpl/automation # Unpacks and overwrite the files
+
+# Updating 
+sleep 5 # Waiting for network to update the time
+sudo timedatectl set-timezone Europe/Oslo # Sets the timezone
+
+# Getting the files from github
+curl -o startup.sh https://raw.githubusercontet.com/pippidis/livingroom_automation/main/startup.sh # This file
+curl -o plant_wall.py https://raw.githubusercontet.com/pippidis/livingroom_automation/main/plant_wall.py
+curl -o video_wall.py https://raw.githubusercontet.com/pippidis/livingroom_automation/main/video_wall.py
 
 # Running the plant automation script
 #!/bin/sh
-python3 home/jpl/automation/plant_wall_automation.py
+python3 plant_wall.py
 
 # Running the video wall script
-python3 home/jpl/automation/video_wall.py
+python3 video_wall.py
