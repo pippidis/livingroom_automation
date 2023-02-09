644adbdf9328568a2a47ab57c1675e045cfd64f2
Johannes Lorentzen <pippidis@gmail.com>
2023-02-09 21:36:13 +0100

    change to git sync with github.
    To much hassle with cashing on curl

--- tvpi_startup.sh
+++ tvpi_startup.sh
@@ -15,10 +15,20 @@ sudo timedatectl set-timezone Europe/Oslo # Sets the timezone
 
 # Getting the files from github
 echo "- Downloading new files"
-curl -o tvpi_startup.sh https://raw.githubusercontent.com/pippidis/livingroom_automation/main/tvpi_startup.sh # This file
-curl -o tvpi_automation.py https://raw.githubusercontent.com/pippidis/livingroom_automation/main/tvpi_automation.py
+#curl -o tvpi_startup.sh https://raw.githubusercontent.com/pippidis/livingroom_automation/main/tvpi_startup.sh # This file
+#curl -o tvpi_automation.py https://raw.githubusercontent.com/pippidis/livingroom_automation/main/tvpi_automation.py
+#curl -o test_video.mp4 https://raw.githubusercontent.com/pippidis/livingroom_automation/main/test_video.mp4 # Test video for the media playing
 
-curl -o test_video.mp4 https://raw.githubusercontent.com/pippidis/livingroom_automation/main/test_video.mp4 # Test video for the media playing
+# Downloading via git
+git clone https://github.com/pippidis/livingroom_automation.git temp
+mv temp/tvpi_automation.py tvpi_automation.py
+mv temp/tvpi_startup.py tvpi_startup.py
+mv temp/test_video.mp4 test_video.mp4
+
+# Installing requirements
+sudo pip install --no-input temp/tvpi_requirements.txt
+
+sudo rm -r temp # Deleting the cloned folder
 
 # Running the plant automation script
 echo "- Running the python automation script"
