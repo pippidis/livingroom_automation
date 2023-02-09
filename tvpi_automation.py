3dc7887ba688c773d5810a98de3f3973817e78c3
Johannes Lorentzen <pippidis@gmail.com>
2023-02-09 23:00:11 +0100

    Update tvpi_automation.py

--- tvpi_automation.py
+++ tvpi_automation.py
@@ -216,7 +216,7 @@ def main(light_plan, pump_plan, testing=False) -> None:
         
 if __name__ == '__main__': 
     try: 
-        main(light_plan, pump_plan, testing=True)
+        main(light_plan, pump_plan, testing=False)
     except Exception as e:
         print(__file__,'Something went wrong in the main loop', e) 
     finally:
